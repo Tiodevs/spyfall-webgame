1f81a89bd7ac8ebb3a887b98cc7f5649e90fe5cc
tiodevs <santospefelipe@gmail.com>
2026-01-22 16:27:49 -0300

    feat(socket): Integração inicial do Socket.io com Express

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Olá, Node.js!');
 });
 
-app.listen(port, () => {
+// Socket.io connection handler
+io.on('connection', (socket) => {
+  console.log(`Usuário conectado: ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    console.log(`Usuário desconectado: ${socket.id}`);
+  });
+});
+
+httpServer.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
